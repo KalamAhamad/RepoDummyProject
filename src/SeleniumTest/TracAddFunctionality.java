960ca24213230b87070795ec6a5689eebdc2a9b3
KalamAhamad <45160562+KalamAhamad@users.noreply.github.com>
2018-11-20 12:47:13 +0530

    Update TracAddFunctionality.java

--- src/SeleniumTest/TracAddFunctionality.java
+++ src/SeleniumTest/TracAddFunctionality.java
@@ -12,7 +12,7 @@ public class TracAddFunctionality {
 	// Static Means particular that class is ready to give access from his side
 	//we can call class by direct as above "TracView.OpenTracView();" but we have to add "static" in TraView class or we can call by object as below, there no required to add static 
 
-	//	TracView obj = new TracView();
+	//TracView obj = new TracView();
    //	obj.OpenTracView();
 	Thread.sleep(1000);
 	 Utility.driver.findElement(By.xpath("//button[text()='View']")).click(); // Click view without selecting a item from grid
